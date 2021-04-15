499551101a62a992f427389a0a8a7bf79cdb8122
Mikhail Hancharou <engeneer2003@mail.ru>
2021-04-16 00:37:50 +0300

    added lab 5

---
+++ Lab5/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car auto = new Car
+            {
+                name = "Tesla Car",
+                TypeOfVehicle = Vehicle.TypesOfVehicle.M,
+                TypeOfColor = Vehicle.TypesOfColor.metalic,
+                Color = Vehicle.Colors.red,
+                TypeOfEngine = Vehicle.TypesOfEngine.electric,
+                TypeOfFuel = Vehicle.TypesOfFuel.energy,
+                EngineCapacity = 90, //kWh or liters
+                AverageFuelСonsumption = 200, //W*h/km or liters
+                AmountOfSeats = 6,
+                YearOfRelease = 2021
+            };
+            auto.SetVin("1294HFJ93JDSD4325");
+            RaceCar sportCar = new RaceCar
+            {
+                name = "Toyota Subaru",
+                TypeOfVehicle = Vehicle.TypesOfVehicle.M,
+                TypeOfColor = Vehicle.TypesOfColor.metalic,
+                Color = Vehicle.Colors.white,
+                TypeOfEngine = Vehicle.TypesOfEngine.internalCombustionEngine,
+                TypeOfFuel = Vehicle.TypesOfFuel.gasoline,
+                EngineCapacity = 4, //kWh or liters
+                AverageFuelСonsumption = 9, //W*h/km or liters
+                AmountOfSeats = 2,
+                YearOfRelease = 2012,
+            };
+            sportCar.SetRate(7.6);
+            sportCar.UpRate(0.1);
+            sportCar.SetRateOfSafety(4.2);
+            sportCar.SetTeamInfo(7.6, "Subaru Masters", "Cederico Dajabello", 5600000, 6, 4, "Toyota", "Toyota");
+            sportCar.SetVin("1264NUTQPXOAM0285");
+            sportCar.GetVehicleInfo();
+            sportCar.GetTeamInfo();
+            Vehicles vehicles = new Vehicles();
+            vehicles[0] = auto;
+            vehicles[1] = sportCar;
+            vehicles.ShowMinInfo(1);
+            vehicles.ShowMinInfo("1294HFJ93JDSD4325");
+            sportCar.HelpInfo();
+            HardTruck truck = new HardTruck
+            {
+                name = "Thunder Truck",
+                TypeOfVehicle = Vehicle.TypesOfVehicle.N,
+                TypeOfColor = Vehicle.TypesOfColor.metalic,
+                Color = Vehicle.Colors.yellow,
+                TypeOfEngine = Vehicle.TypesOfEngine.internalCombustionEngine,
+                TypeOfFuel = Vehicle.TypesOfFuel.petrol,
+                EngineCapacity = 320, //kWh or liters
+                AverageFuelСonsumption = 20, //W*h/km or liters
+                YearOfRelease = 2020,
+                Weight = 7250,
+                TypeOfTruck = HardTruck.TypesOfTruck.pileDrivingMachines
+            };
+            truck.SetVin("6842AMCHRYSSI8214");
+            truck.SetWeightPossibilities(12000);
+            truck.UpWeightPossibilities(1000);
+            truck.AddSkill(HardTruck.TypesOfTruck.pipeLayingMachines);
+            truck.GetVehicleInfo();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab5/Vehicle.cs
@@ -0,0 +1,158 @@
+﻿using System;
+
+namespace Lab5
+{
+    public abstract class Vehicle
+    {
+        public enum TypesOfVehicle
+        {
+            L,
+            M,
+            N,
+            O,
+            undefined
+        }
+
+        public enum TypesOfColor
+        {
+            metalic,
+            nonmetalic,
+            undefined
+        }
+
+        public enum Colors
+        {
+            blue,
+            yellow,
+            grey,
+            white,
+            black,
+            brown,
+            green,
+            red,
+            purple,
+            pink,
+            undefined
+        }
+
+        public enum TypesOfEngine
+        {
+            internalCombustionEngine,
+            electric,
+            undefined
+        }
+
+        public enum TypesOfFuel
+        {
+            petrol,
+            gasoline,
+            energy,
+            kerosene,
+            diesel,
+            gas,
+            undefined
+        }
+
+        public string name { get; set; }
+        public TypesOfVehicle TypeOfVehicle { get; set; }
+        public TypesOfColor TypeOfColor { get; set; }
+        public Colors Color { get; set; }
+        public TypesOfEngine TypeOfEngine { get; set; }
+        public TypesOfFuel TypeOfFuel { get; set; }
+        public double EngineCapacity { get; set; }
+        public double AverageFuelСonsumption { get; set; }
+        public int YearOfRelease { get; set; }
+        public string Vincode { get; private set; }
+
+        public Vehicle()
+        {
+            name = null;
+            TypeOfVehicle = TypesOfVehicle.undefined;
+            TypeOfColor = TypesOfColor.undefined;
+            Color = Colors.undefined;
+            TypeOfEngine = TypesOfEngine.undefined;
+            TypeOfFuel = TypesOfFuel.undefined;
+            EngineCapacity = 0;
+            AverageFuelСonsumption = 0;
+            YearOfRelease = 0;
+            Vincode = null;
+        }
+
+        public Vehicle(string nameCar, TypesOfVehicle typesOfVehicle, TypesOfColor typesOfColor, Colors colors, TypesOfEngine typesOfEngine, TypesOfFuel typesOfFuel, double engineCapacity, double averageFuelСonsumption, int yearOfRelease, string vincode)
+        {
+            name = nameCar;
+            TypeOfVehicle = typesOfVehicle;
+            TypeOfColor = typesOfColor;
+            Color = colors;
+            TypeOfEngine = typesOfEngine;
+            TypeOfFuel = typesOfFuel;
+            EngineCapacity = engineCapacity;
+            AverageFuelСonsumption = averageFuelСonsumption;
+            YearOfRelease = yearOfRelease;
+            Vincode = vincode;
+        }
+
+        public Vehicle(string nameCar, TypesOfVehicle typesOfVehicle, TypesOfColor typesOfColor, Colors colors, TypesOfEngine typesOfEngine, TypesOfFuel typesOfFuel)
+        {
+            name = nameCar;
+            TypeOfVehicle = typesOfVehicle;
+            TypeOfColor = typesOfColor;
+            Color = colors;
+            TypeOfEngine = typesOfEngine;
+            TypeOfFuel = typesOfFuel;
+        }
+
+        public Vehicle(string nameCar, double engineCapacity, double averageFuelСonsumption, int yearOfRelease, string vincode)
+        {
+            name = nameCar;
+            EngineCapacity = engineCapacity;
+            AverageFuelСonsumption = averageFuelСonsumption;
+            YearOfRelease = yearOfRelease;
+            Vincode = vincode;
+        }
+
+        public void SetVin(string vin)
+        {
+            Vincode = vin;
+        }
+
+        public virtual void HelpInfo()
+        {
+            Console.WriteLine(@"
+                TypesOfVehicle(L, M, N, O); 
+                TypesOfColor(metalic, nonmetalic); 
+                Colors(blue, yellow, grey, white, black, brown, green, red, purple, pink);
+                TypesOfEngine(internalCombustionEngine, electric);
+                TypesOfFuel(petrol, gasoline, energy, kerosene, diesel, gas);
+                -----------------------------------------------------------------------------------------------
+                First constructor get: string nameCar, TypesOfVehicle typesOfVehicle, TypesOfColor typesOfColor,
+                    Colors colors, TypesOfEngine typesOfEngine, TypesOfFuel typesOfFuel, double engineCapacity,
+                    double averageFuelСonsumption, int yearOfRelease, string vincode;
+                -----------------------------------------------------------------------------------------------
+                Second constructor get: string nameCar, TypesOfVehicle typesOfVehicle, TypesOfColor typesOfColor,
+                    Colors colors, TypesOfEngine typesOfEngine, TypesOfFuel typesOfFuel;
+                -----------------------------------------------------------------------------------------------
+                Third constructor get: string nameCar, double engineCapacity, double averageFuelСonsumption, 
+                    int yearOfRelease, string vincode;
+                -----------------------------------------------------------------------------------------------
+                You can get vehicle info by using 'GetVehicleInfo';
+                You can set vin code by using 'SetVin';
+                ");
+        }
+
+        public virtual void GetVehicleInfo()
+        {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Type of vehicle: {TypeOfVehicle}");
+            Console.WriteLine($"Type of color: {TypeOfColor}");
+            Console.WriteLine($"Color: {Color}");
+            Console.WriteLine($"Type of engine: {TypeOfEngine}");
+            Console.WriteLine($"Type of fuel: {TypeOfFuel}");
+            Console.WriteLine($"Engine capacity: {EngineCapacity}");
+            Console.WriteLine($"Average fuel consumption: {AverageFuelСonsumption}");
+            Console.WriteLine($"Year of release: {YearOfRelease}");
+            Console.WriteLine($"Vin code: {Vincode}");
+            Console.WriteLine("---------------------\n");
+        }
+    }
+}
